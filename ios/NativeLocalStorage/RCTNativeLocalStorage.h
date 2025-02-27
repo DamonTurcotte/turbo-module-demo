8d3a61284fa480812b25f120a8266f5ec4a61fd1
Damon Turcotte <damon.j.turcotte@gmail.com>
2025-02-27 00:19:46 -0700

    feat: add RCTNativeLocalStorage header file for TurboModule integration

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleDemo
+//
+//  Created by Damon Turcotte on 2025-02-26.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
